8eab60e209072e535b2dbb94769b233854ed43f4
Ethan Sutherland <ESutherland95@gmail.com>
2024-04-02 18:45:17 -0500

    created help command

--- src/commands/misc/help.ts
+++ src/commands/misc/help.ts
@@ -1,4 +1,11 @@
-import { Client, CommandInteraction, EmbedBuilder } from "discord.js";
+import {
+  Client,
+  CommandInteraction,
+  EmbedBuilder,
+  PermissionFlagsBits,
+  PermissionsBitField,
+} from "discord.js";
+import { getLocalCommands } from "../../util/get_local_commands";
 
 export default {
   data: {
@@ -7,6 +14,35 @@ export default {
   },
 
   callback: async (client: Client, interaction: CommandInteraction) => {
-    interaction.reply("NOT IMPLEMENTED YET!");
+    const commands = (await getLocalCommands()).sort((a, b) => {
+      if (a.data.name < b.data.name) return -1;
+      if (a.data.name > b.data.name) return 1;
+      return -1;
+    });
+
+    const embed = new EmbedBuilder()
+      .setTitle("Commands")
+      .setDescription(
+        commands
+          .map((command) => {
+            let permissions: string[] = [];
+            Object.entries(PermissionsBitField.Flags).forEach(
+              ([key, value]) => {
+                command.permissionsRequired?.forEach((permission: bigint) => {
+                  if (value === permission) permissions.push(key);
+                });
+              }
+            );
+            return `/**${command.data.name}** - ${command.data.description} ${
+              permissions.length > 0
+                ? `| **Permissions Required**: ${permissions.join(", ")}`
+                : ""
+            }`;
+          })
+          .join("\n")
+      )
+      .setColor(interaction.guild?.members.me?.displayColor || "Random");
+
+    await interaction.reply({ embeds: [embed] });
   },
 };
