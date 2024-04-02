afe485d8057aec7a4527a9da17c617e3c424dce9
Ethan Sutherland <ESutherland95@gmail.com>
2024-04-02 08:43:53 -0500

    updates

--- dist/src/util/calculate_level_xp.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (level) => 100 * level * 0.8 || 1;
--- dist/src/util/data.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.commandMessageData = exports.pokemonMessageData = void 0;
-exports.pokemonMessageData = [];
-exports.commandMessageData = [];
